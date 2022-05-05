956e714bad53e07ec7fa48ccb206f198716185f9
James Bradley <jpbradl@g.clemson.edu>
2022-05-05 09:38:07 -0400

    adding challenges from 5/5/22

---
+++ 050522/identicalElements.js
@@ -0,0 +1,13 @@
+
+// Given two arrays of integers m and n, test if they contain at least one identical element. 
+// Return true if they do; false if not.
+
+// Your code must handle any value within the range of a 32-bit integer, 
+// and must be capable of handling either array being empty (which is a false result, 
+// as there are no duplicated elements).
+
+
+function duplicateElements(m, n) {
+  return m === [] || n === [] ? false : 
+         m.map(element => n.includes(element)).some(Boolean);  
+}
