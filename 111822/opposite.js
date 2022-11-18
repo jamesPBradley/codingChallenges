3f523b1fcd852d2dbd50f90fa034ac8ab5953d86
James Bradley <jpbradl@g.clemson.edu>
2022-11-18 18:58:44 -0500

    adding challenges from 11/18/22

---
+++ 111822/opposite.js
@@ -0,0 +1,14 @@
+// p: num
+// r: num
+
+function opposite(n) {
+    return 0-n;
+}
+
+
+console.log(opposite(1), -1);
+console.log(opposite(0), 0);
+console.log(opposite(4.25), -4.25);
+console.log(opposite(3.3333333), -3.3333333);
+console.log(opposite(-12525220.3325), 12525220.3325);
+console.log(opposite(-5), 5);
