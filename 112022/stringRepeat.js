c5a1c18cabfececd3d54ac30a3d0395dce43b97c
James Bradley <jpbradl@g.clemson.edu>
2022-11-20 07:56:58 -0500

    adding challenges from 11/20/22

---
+++ 112022/stringRepeat.js
@@ -0,0 +1,10 @@
+// p: number, string
+// r: string
+
+function repeatStr(n,str) {
+    return str.repeat(n);
+}
+
+console.log(repeatStr(3, "*"), "***");
+console.log(repeatStr(5, "#"), "#####");
+console.log(repeatStr(2, "ha "), "ha ha ");
