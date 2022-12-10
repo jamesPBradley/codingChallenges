e8f5d2057c50912564c9beac32d3108f3869b918
James Bradley <jpbradl@g.clemson.edu>
2022-12-10 07:26:21 -0500

    adding challenges from 12/10/22

---
+++ 121022/bonusTime.js
@@ -0,0 +1,14 @@
+// p: number, boolean
+// r: string
+
+function bonusTime(salary, bonus) {
+    return `£${bonus ? salary*10 : salary}`;
+}
+
+console.log(bonusTime(10000, true), '£100000');
+console.log(bonusTime(25000, true), '£250000');
+console.log(bonusTime(10000, false), '£10000');
+console.log(bonusTime(60000, false), '£60000');
+console.log(bonusTime(2, true), '£20');
+console.log(bonusTime(78, false), '£78');
+console.log(bonusTime(67890, true), '£678900');
