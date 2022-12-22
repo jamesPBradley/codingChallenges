6d02f2e829b6e23babb68d7e7636d8e36ce14b70
James Bradley <jpbradl@g.clemson.edu>
2022-12-22 14:21:47 -0500

    adding challenges from 12/22/22

---
+++ 122222/syntax.js
@@ -0,0 +1,9 @@
+// p: string, string
+// r: string
+
+function main (s1,s2) {
+    return `${s1}${s2}`;
+}
+
+console.log(main('take ', 'item'), 'take item');
+console.log(main('use ', 'sword'), 'use sword');
