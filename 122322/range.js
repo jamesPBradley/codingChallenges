3e0fe126b2c52876c5472cfbe60d89e7d7c3bf98
James Bradley <jpbradl@g.clemson.edu>
2022-12-23 08:15:46 -0500

    adding challenges from 12/23/22

---
+++ 122322/range.js
@@ -0,0 +1,12 @@
+// p: num,num,num
+// r: array
+
+function generateRange(min,max,step) {
+    let arr = [];
+    for(let i=min; i<=max; i+=step) {
+        arr.push(i);
+    }
+    return arr;
+}
+
+console.log(generateRange(2, 10, 2), [2,4,6,8,10]);
