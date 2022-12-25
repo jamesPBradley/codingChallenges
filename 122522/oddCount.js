b1b38456f57cd103493af8cde65f2c9391a764fc
James Bradley <jpbradl@g.clemson.edu>
2022-12-25 10:32:01 -0500

    adding challenges from 12/25/22

---
+++ 122522/oddCount.js
@@ -0,0 +1,9 @@
+// p: number
+// r: number
+
+function oddCount(n) {
+    return Math.floor(n/2);
+}
+
+console.log(oddCount(15), 7, "Oops! Wrong.");
+console.log(oddCount(15023), 7511, "Oops! Wrong.");
