0246caab86f7dd2b3754bdb91925caf51203b193
James Bradley <jpbradl@g.clemson.edu>
2023-01-06 18:37:25 -0500

    adding challenges from 1/6/23

---
+++ 010623/toBinary.js
@@ -0,0 +1,10 @@
+// p: number
+// r: string
+
+function toBinary(n) {
+    return +n.toString(2);;
+}
+
+console.log(toBinary(1));  /* should return 1 */
+console.log(toBinary(5));  /* should return 101 */
+console.log(toBinary(11)); /* should return 1011 */
