e3c2c3e194645373afdfbc963f0bdc8a984f934f
James Bradley <jpbradl@g.clemson.edu>
2023-01-16 09:46:42 -0500

    adding challenges from 1/16/23

---
+++ 011623/arithmetic.js
@@ -0,0 +1,14 @@
+// p: num,num,string
+// r: num
+
+function arithmetic(a,b,operator) {
+    return operator=='add' ? a+b :
+           operator=='subtract' ? a-b :
+           operator=='multiply' ? a*b : a/b;
+}
+
+
+console.log(arithmetic(1, 2, "add"), 3, "'add' should return the two numbers added together!");
+console.log(arithmetic(8, 2, "subtract"), 6, "'subtract' should return a minus b!");
+console.log(arithmetic(5, 2, "multiply"), 10, "'multiply' should return a multiplied by b!");
+console.log(arithmetic(8, 2, "divide"), 4, "'divide' should return a divided by b!");
